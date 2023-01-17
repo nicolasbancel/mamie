8c77e95213231a0cb3d03da905563a923b2419c2
Nicolas Bancel <nicolas.bancel@gmail.com>
2023-01-17 17:18:32 +0100

    refacto code angles

--- project/angles.py
+++ project/angles.py
@@ -3,6 +3,8 @@
 ## Working on mamie0008.jpg
 ## Working on mamie0030.jpg as well
 
+## mamie0024.jpg
+
 from final import *
 from statistics import mean
 from shapely.geometry import Polygon, LineString
@@ -24,12 +26,12 @@ def get_angles(contour):
     b = np.roll(a, -1, axis=0)
     alengths = np.linalg.norm(a, axis=1)
     blengths = np.linalg.norm(b, axis=1)
-    MAX_SIDE_LENGTH = alengths.max()
+    max_side_length = alengths.max()
     crossproducts = np.cross(a, b) / alengths / blengths
     angles = np.arcsin(crossproducts)
     angles_degrees = angles / np.pi * 180
 
-    return angles_degrees, MAX_SIDE_LENGTH, alengths, blengths
+    return angles_degrees, max_side_length, alengths, blengths
 
 
 def plot_angles(contour, angles_degrees):
@@ -64,7 +66,7 @@ def enrich_contour_info(contour, angles_degrees, alengths, blengths):
         a_line = float(point[3])
         b_line = float(point[4])
         # Index -1 is to determine if this is a good point, a scission point, or a bad point
-        length_thresh = MAX_SIDE_LENGTH * THRESHOLD
+        length_thresh = max_side_length * THRESHOLD
         if angle < SMALL_ANGLE_THRESH:
             if a_line > length_thresh and b_line > length_thresh:
                 enriched_contour[index][-1] = "scission"
@@ -82,10 +84,10 @@ def enrich_contour_info(contour, angles_degrees, alengths, blengths):
         after = np.asarray(scission["after_scission_point"], dtype=int)
         middle_point = [mean([before[0], after[0]]), mean([before[1], after[1]])]
 
-    return enriched_contour, scission_information, middle_point
+    return enriched_contour, scission_information, middle_point, scission_point
 
 
-def plot_points(angle_degrees, enriched_contour, contour):
+def plot_points(angle_degrees, enriched_contour, contour, middle_point):
     cv_rows = 6000
     cv_columns = 6000
 
@@ -104,149 +106,20 @@ def plot_points(angle_degrees, enriched_contour, contour):
 
     show("Canvas", cv)
 
-if __name__ == '__main__':
-    
-    # INTRO STEPS - LOADING DATA
-    original, original_with_main_contours, PictureContours, message = final_steps(picture_name="mamie0030.jpg", THRESH_MIN=245, THESH_MAX=255)
-    contour = PictureContours[0][0]
-    test = original.copy()
-    cv2.drawContours(test, [contour], -1, (0, 255, 0), 40)
-    show("Contour", test)
-    
-    # GETTING ANGLES
-    angles_degrees, MAX_SIDE_LENGTH, alengths, blengths = get_angles(contour)
-    # plot_angles(contour, angles_degrees)
-    enriched_contour, scission_information = enrich_contour_info(contour, angles_degrees, alengths, blengths)
+    return cv
 
 
-
-
-
-
-
-
-
-
-    # def get_angles(contour):
-    # funny shape because OpenCV. it's a Nx1 vector of 2-channel elements
-    # fix that up, remove the silly dimension
-
-    num_point = len(contour)
-    contour.shape = (-1, 2)
-
-    # the vectors are differences of coordinates
-    # a points into the point, b out of the point
-    a = contour - np.roll(contour, 1, axis=0)
-    b = np.roll(a, -1, axis=0)  # same but shifted
-
-    # we'll need to know the length of those vectors
-    alengths = np.linalg.norm(a, axis=1)
-    blengths = np.linalg.norm(b, axis=1)
-
-    # we need only the length of the cross product,
-    # and we work in 2D space anyway (not 3D),
-    # so the cross product can't result in a vector, just its z-component
-    crossproducts = np.cross(a, b) / alengths / blengths
-
-    angles = np.arcsin(crossproducts)
-    angles_degrees = angles / np.pi * 180
-
-    # print("angles in degrees:")
-    # print(angles_degrees)
-
-    point_category = np.transpose([["good"] * num_point])
-
-    # this is just for printing/displaying, not useful in code
-    # print("point and angle:")
-    enriched_contour = np.hstack([contour, angles_degrees.reshape((-1, 1)), alengths.reshape((-1, 1)), blengths.reshape((-1, 1)), point_category])
-    # print(enriched_contour)
-
-
-    canvas_rows = 6000
-    canvas_columns = 6000
-
-    canvas = np.zeros((canvas_rows, canvas_columns, 3))  # floats, range 0..1
-    canvas_original = canvas.copy()
-
-    # https://stackoverflow.com/questions/19103933/depth-error-in-2d-image-with-opencv-python
-    canvas_copy = np.uint8(canvas)
-    new_canvas = (canvas * 255).astype(np.uint8)
-
-    cv2.polylines(new_canvas, [contour], isClosed=True, color=(1, 1, 1))
-
-    # fontScale=20 // thickness=5 : thickness is good, fontScale is way too big
-    # fontScale=7 // thickness=5 :
-
-    for i, angle in enumerate(angles_degrees):
-        cv2.circle(new_canvas, center=tuple(contour[i]), radius=20, color=(0, 0, 1), thickness=cv2.FILLED)
-        cv2.putText(new_canvas, f"{angle:+.1f}", org=tuple(contour[i]), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=7, color=(0, 1, 1), thickness=5)
-
-    show("Canvas", new_canvas)
-
-
-    ## Identify bad points vs points for scission between
-
-    # Maximum length of the sides of the polygon
-    MAX_SIDE_LENGTH = alengths.max()
-    THRESHOLD = 0.25
-
-
-    scission_information = []
-    scission_dict = dict()
-
-    for index, point in enumerate(enriched_contour):
-        x_coord = point[0]
-        y_coord = point[1]
-        angle = abs(float(point[2]))
-        a_line = float(point[3])
-        b_line = float(point[4])
-        # Index -1 is to determine if this is a good point, a scission point, or a bad point
-        length_thresh = MAX_SIDE_LENGTH * THRESHOLD
-        if angle < SMALL_ANGLE_THRESH:
-            if a_line > length_thresh and b_line > length_thresh:
-                enriched_contour[index][-1] = "scission"
-                scission_dict["scission_point"] = list([x_coord, y_coord])
-                scission_dict["before_scission_point"] = list([enriched_contour[index - 1][0], enriched_contour[index - 1][1]])
-                scission_dict["after_scission_point"] = list([enriched_contour[index + 1][0], enriched_contour[index + 1][1]])
-                scission_information.append(scission_dict)
-            else:
-                enriched_contour[index][-1] = "bad"
-
-
-    if len(scission_information) >= 1:
-        scission = scission_information[0]
-        scission_point = np.asarray(scission["scission_point"], dtype=int)
-        before = np.asarray(scission["before_scission_point"], dtype=int)
-        after = np.asarray(scission["after_scission_point"], dtype=int)
-        middle_point = [mean([before[0], after[0]]), mean([before[1], after[1]])]
-
-
-    cv_rows = 6000
-    cv_columns = 6000
-
-    cv = np.zeros((cv_rows, cv_columns, 3))  # floats, range 0..1
-    cv2.polylines(cv, [contour], isClosed=True, color=(1, 1, 1))
-
-    for i, angle in enumerate(angles_degrees):
-        if enriched_contour[i][-1] == "bad":
-            cv2.circle(cv, center=tuple(contour[i]), radius=20, color=(0, 0, 1), thickness=cv2.FILLED)
-        elif enriched_contour[i][-1] == "scission":
-            cv2.circle(cv, center=tuple(contour[i]), radius=20, color=(1, 0, 0), thickness=cv2.FILLED)
-        else:
-            cv2.circle(cv, center=tuple(contour[i]), radius=20, color=(0, 1, 0), thickness=cv2.FILLED)
-    # This prints the point which is in the middle of the 2 scission points
-    cv2.circle(cv, center=tuple(middle_point), radius=20, color=(42, 35, 9), thickness=cv2.FILLED)
-
-
-    show("Canvas", cv)
-
-    ###### FINDING INTERSECTION
-
-    # https://stackoverflow.com/questions/47359985/shapely-intersection-point-between-line-and-polygon-in-3d
-    # https://stackoverflow.com/questions/62388276/how-to-create-an-infinite-line-from-two-given-points-to-get-intersection-with-ot
+def find_extrapolation(middle_point, scission_point):
+    """
+    For a given massive contour (interpreted as a polygon), with a scission point
+    The function determines the scission line associated, which would split the polygon into 2 parts
+    2 steps :
+    - Builds the intersection line - finds its parameters / equation
+    - Identifies the intersection point between the scission line and the polygon
+    - Splits the polygon
+    """
 
     line = LineString([middle_point, scission_point])
-    polygon = Polygon(contour)
 
     # Line has equation
     # Y = k*x + m
@@ -269,7 +142,7 @@ if __name__ == '__main__':
     # xc
 
     xc = symbols("xc")
-    eq = Eq(((k * xc + m) - yb) ** 2 + (xc - xb) ** 2 - MAX_SIDE_LENGTH**2)
+    eq = Eq(((k * xc + m) - yb) ** 2 + (xc - xb) ** 2 - max_side_length**2, 0)
     solutions = solve(eq)
 
     xc1 = solutions[0]
@@ -282,13 +155,14 @@ if __name__ == '__main__':
 
     c2 = [int(xc2), int(yc2)]
 
+    ## There are 2 solutions to the equation (2 end points)
+    # One "after" the scission point, in the opposite side of the middle point :
+    #   - That's the one we want to keep : it will intersect with the polygon
+    # One "before" the scission point, in the same direction as the middle point
+    #   - That one will not intersect with the polygon : it will, but just at the scission point, which is redundant)
 
-    # cv2.circle(cv, center=tuple(c1), radius=20, color=(42, 35, 9), thickness=cv2.FILLED)
-    # cv2.circle(cv, center=tuple(c2), radius=20, color=(42, 35, 9), thickness=cv2.FILLED)
-
-    # Vector : scission - middle
-    # Need to keep the vector that goes in the same direction as vector_ref (scission - middle)
-
+    # Reference vector is thus the direction of middle -> scission
+    # We want the scission -> extrapolated to follow the same direction
     vector_ref = scission_point - middle_point
     vector_c1 = c1 - scission_point
     vector_c2 = c2 - scission_point
@@ -298,22 +172,28 @@ if __name__ == '__main__':
     else:
         extrapolated_point = c2
 
+    return extrapolated_point
+
+
+def split_contour(contour, extrapolated_point, scission_point, middle_point, original, canvas=None):
+    """
+    - Determine whether or not the line intersects the polygon
+        - Documentation : https://stackoverflow.com/questions/6050392/determine-if-a-line-segment-intersects-a-polygon
+    - Determine the intersection point + draws the splitting line
+    - Splits the polygon in halft
+    """
+
     new_line = LineString([scission_point, extrapolated_point])
+    polygon = Polygon(contour)
 
-    cv2.line(cv, scission_point, extrapolated_point, (0, 255, 0), thickness=7)
-    show("Canvas", cv)
-
-    # Determine whether or not the line intersects the polygon
-    # https://stackoverflow.com/questions/6050392/determine-if-a-line-segment-intersects-a-polygon
-    # new_line.intersects(polygon) : True
-
+    # FIND THE INTERSECTION
 
     intersections = new_line.intersection(polygon)
+
     # There are 2 intersections. 1 is the scission point (since it's the starting point).
     # The other is the interesting point
 
     first_intersection = np.array([intersections.boundary.geoms[0].x, intersections.boundary.geoms[0].y], dtype=int)
-    # first_intersection == scission_point
     second_intersection = np.array([intersections.boundary.geoms[1].x, intersections.boundary.geoms[1].y], dtype=int)
 
     if (first_intersection == scission_point).all():
@@ -321,39 +201,52 @@ if __name__ == '__main__':
     else:
         intersection_point = first_intersection
 
-    # intersection_point = np.asarray([inter.x, inter.y], dtype=int)
+    if canvas is not None:
+        cv2.line(canvas, scission_point, extrapolated_point, (0, 255, 0), thickness=7)
+        cv2.circle(canvas, center=tuple(second_intersection), radius=20, color=(42, 35, 9), thickness=cv2.FILLED)
+        show("Canvas", canvas)
 
-    cv2.circle(cv, center=tuple(second_intersection), radius=20, color=(42, 35, 9), thickness=cv2.FILLED)
+    splitting_line = LineString([middle_point, intersection_point])
 
-    # inter = line.intersection(polygon)
+    # SPLIT THE POLYGON
 
-    show("Canvas", cv)
-
-    ## SPLITTING THE POLYGON INTO 2 PARTS
-
-    splitting_line = LineString([middle_point, intersection_point])  ## This should split the polygon in 2 (and interesect with it)
-    merged = linemerge([polygon.boundary, splitting_line])
+    # SPLITTING LINE IS USELESS, NO ? VS NEW_LINE...
+    # merged = linemerge([polygon.boundary, splitting_line])
+    merged = linemerge([polygon.boundary, new_line])
     borders = unary_union(merged)
     polygons = list(polygonize(borders))
 
-    polygons_array = [np.array(list(pol.exterior.coords), dtype=int) for pol in polygons]
+    new_contours = [np.array(list(pol.exterior.coords), dtype=int) for pol in polygons]
 
-    # Green, then red, then blue
     color_list = [(0, 255, 0), (0, 0, 255), (255, 0, 0)]
 
     original_copy = original.copy()
 
-    for index, p in polygons_array:
+    for index, p in enumerate(new_contours):
+        print(f"Number of points in contour : {len(p)}")
+        print(p)
         cv2.drawContours(original_copy, [p], -1, color_list[index], 40)
+        for point in p:
+            cv2.circle(original_copy, center=tuple(point), radius=20, color=(1, 0, 0), thickness=cv2.FILLED)
 
+    show("New contours", original_copy)
 
-    # cv2.drawContours(original_copy, [first_contour], -1, color_list[0], 40)
+    return new_contours, intersection_point
 
 
 if __name__ == "__main__":
 
-    
+    # INTRO STEPS - LOADING DATA
+    original, original_with_main_contours, PictureContours, message = final_steps(picture_name="mamie0030.jpg", THRESH_MIN=245, THESH_MAX=255)
+    contour = PictureContours[0][0]
+    test = original.copy()
+    cv2.drawContours(test, [contour], -1, (0, 255, 0), 40)
+    show("Contour", test)
 
-    ### Taken directly from : https://stackoverflow.com/questions/68971669/calculate-inner-angles-of-a-polygon
-
-    ### Test
+    # GETTING ANGLES
+    angles_degrees, max_side_length, alengths, blengths = get_angles(contour)
+    # plot_angles(contour, angles_degrees)
+    enriched_contour, scission_information, middle_point, scission_point = enrich_contour_info(contour, angles_degrees, alengths, blengths)
+    cv = plot_points(angles_degrees, enriched_contour, contour, middle_point)
+    extrapolated_point = find_extrapolation(middle_point, scission_point, cv)
+    new_contours, intersection_point = split_contour(contour, extrapolated_point, scission_point, middle_point, original, cv)
