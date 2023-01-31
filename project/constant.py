dbd5c51fc4c3db1224cbd85acd83fbf80771a2b6
Nicolas Bancel <nicolas.bancel@gmail.com>
2023-01-31 12:12:50 +0100

    README

--- project/constant.py
+++ project/constant.py
@@ -131,8 +131,8 @@ OPENCV_DATA_DIR = "/Users/nicolasbancel/git/perso/mamie/data/opencv/"
 # FACE DETECTION MODELS
 #######################################################################
 
-YUNET_PATH = "/Users/nicolasbancel/git/perso/mamie/data/opencv_dnn/face_detection_yunet_2022mar.onnx"
-SFACE_PATH = "/Users/nicolasbancel/git/perso/mamie/data/opencv_dnn/face_recognition_sface_2021dec.onnx"
+YUNET_PATH = "/Users/nicolasbancel/git/perso/mamie/data/opencv/face_detection_yunet_2022mar.onnx"
+SFACE_PATH = "/Users/nicolasbancel/git/perso/mamie/data/opencv/face_recognition_sface_2021dec.onnx"
 
 
 # MOSAIC_DIR = os.path.join(Path.cwd().parent, "data/mosaic/")
